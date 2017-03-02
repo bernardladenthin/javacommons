82c53365943b0b526202ce20fad7db89bf351aaa
Bernard Ladenthin <bernard.ladenthin@gmail.com>
2017-03-02 12:40:38 +0100

    improve StringCollectionToPrimitiveArray and StringCollectionToPrimitiveArrayTest

--- src/main/java/net/ladenthin/javacommons/StringCollectionToPrimitiveArray.java
+++ src/main/java/net/ladenthin/javacommons/StringCollectionToPrimitiveArray.java
@@ -19,104 +19,222 @@
 package net.ladenthin.javacommons;
 
 import java.util.Collection;
-import java.util.Iterator;
 
 /**
- * Convert a string collection to an array containing primitive data types.
+ * Convert a string collection to an array containing string or primitive data types.
  * All methods are thread safe.
  *
  * @author Bernard Ladenthin bernard.ladenthin@gmail.com
  */
 public class StringCollectionToPrimitiveArray {
 
+    /**
+     * An empty string array.
+     */
+    public static final String[] EMPTY_ARRAY_STRING = new String[0];
+
+    /**
+     * An empty byte array.
+     */
+    public static final byte[] EMPTY_ARRAY_BYTE = new byte[0];
+
+    /**
+     * An empty short array.
+     */
+    public static final short[] EMPTY_ARRAY_SHORT = new short[0];
+
+    /**
+     * An empty int array.
+     */
+    public static final int[] EMPTY_ARRAY_INT = new int[0];
+
+    /**
+     * An empty long array.
+     */
+    public static final long[] EMPTY_ARRAY_LONG = new long[0];
+
+    /**
+     * An empty float array.
+     */
+    public static final float[] EMPTY_ARRAY_FLOAT = new float[0];
+
+    /**
+     * An empty double array.
+     */
+    public static final double[] EMPTY_ARRAY_DOUBLE = new double[0];
+
+    /**
+     * An empty boolean array.
+     */
+    public static final boolean[] EMPTY_ARRAY_BOOLEAN = new boolean[0];
+
+    /**
+     * An empty char array.
+     */
+    public static final char[] EMPTY_ARRAY_CHAR = new char[0];
+
+    /**
+     * Convert a {@link String} {@link Collection} to a {@link String} array.
+     * No further converts.
+     * <code>null</code> values in the collection are allowed and will be <code>null</code> values in the array.
+     * @param strings The {@link String}s. Can be <code>null</code>.
+     * @return The {@link String} array. Never be <code>null</code>, may be empty.
+     */
     public String[] stringCollectionToStringArray(Collection<String> strings) {
+        if(strings == null || strings.size() == 0) {
+            return EMPTY_ARRAY_STRING;
+        }
         return strings.toArray(new String[strings.size()]);
     }
 
+    /**
+     * Convert a {@link String} {@link Collection} containing bytes to a {@link String} array.
+     * Convert all entries using {@link Byte#parseByte(String)}.
+     * <code>null</code> values in the collection are not allowed and throw a {@link NumberFormatException}.
+     * @param strings The {@link String}s. Can be <code>null</code>.
+     * @return The byte array. Never be <code>null</code>, may be empty.
+     */
     public byte[] stringCollectionToByteArray(Collection<String> strings) {
+        if(strings == null || strings.size() == 0) {
+            return EMPTY_ARRAY_BYTE;
+        }
         byte[] ret = new byte[strings.size()];
         int i = 0;
-        for (Iterator<String> it = strings.iterator(); it.hasNext(); ) {
-            String string = it.next();
-            ret[i] = Byte.parseByte(string);
-            i++;
+        for (String string : strings) {
+            ret[i++] = Byte.parseByte(string);
         }
         return ret;
     }
 
+    /**
+     * Convert a {@link String} {@link Collection} containing shorts to a {@link String} array.
+     * Convert all entries using {@link Short#parseShort(String)}.
+     * <code>null</code> values in the collection are not allowed and throw a {@link NumberFormatException}.
+     * @param strings The {@link String}s. Can be <code>null</code>.
+     * @return The short array. Never be <code>null</code>, may be empty.
+     */
     public short[] stringCollectionToShortArray(Collection<String> strings) {
+        if(strings == null || strings.size() == 0) {
+            return EMPTY_ARRAY_SHORT;
+        }
         short[] ret = new short[strings.size()];
         int i = 0;
-        for (Iterator<String> it = strings.iterator(); it.hasNext(); ) {
-            String string = it.next();
-            ret[i] = Short.parseShort(string);
-            i++;
+        for (String string : strings) {
+            ret[i++] = Short.parseShort(string);
         }
         return ret;
     }
 
+    /**
+     * Convert a {@link String} {@link Collection} containing ints to a {@link String} array.
+     * Convert all entries using {@link Integer#parseInt(String)}.
+     * <code>null</code> values in the collection are not allowed and throw a {@link NumberFormatException}.
+     * @param strings The {@link String}s. Can be <code>null</code>.
+     * @return The int array. Never be <code>null</code>, may be empty.
+     */
     public int[] stringCollectionToIntArray(Collection<String> strings) {
+        if(strings == null || strings.size() == 0) {
+            return EMPTY_ARRAY_INT;
+        }
         int[] ret = new int[strings.size()];
         int i = 0;
-        for (Iterator<String> it = strings.iterator(); it.hasNext(); ) {
-            String string = it.next();
-            ret[i] = Integer.parseInt(string);
-            i++;
+        for (String string : strings) {
+            ret[i++] = Integer.parseInt(string);
         }
         return ret;
     }
 
+    /**
+     * Convert a {@link String} {@link Collection} containing longs to a {@link String} array.
+     * Convert all entries using {@link Long#parseLong(String)}.
+     * <code>null</code> values in the collection are not allowed and throw a {@link NumberFormatException}.
+     * @param strings The {@link String}s. Can be <code>null</code>.
+     * @return The long array. Never be <code>null</code>, may be empty.
+     */
     public long[] stringCollectionToLongArray(Collection<String> strings) {
+        if(strings == null || strings.size() == 0) {
+            return EMPTY_ARRAY_LONG;
+        }
         long[] ret = new long[strings.size()];
         int i = 0;
-        for (Iterator<String> it = strings.iterator(); it.hasNext(); ) {
-            String string = it.next();
-            ret[i] = Long.parseLong(string);
-            i++;
+        for (String string : strings) {
+            ret[i++] = Long.parseLong(string);
         }
         return ret;
     }
 
+    /**
+     * Convert a {@link String} {@link Collection} containing floats to a {@link String} array.
+     * Convert all entries using {@link Float#parseFloat(String)}.
+     * <code>null</code> values in the collection are not allowed and throw a {@link NullPointerException}.
+     * @param strings The {@link String}s. Can be <code>null</code>.
+     * @return The float array. Never be <code>null</code>, may be empty.
+     */
     public float[] stringCollectionToFloatArray(Collection<String> strings) {
+        if(strings == null || strings.size() == 0) {
+            return EMPTY_ARRAY_FLOAT;
+        }
         float[] ret = new float[strings.size()];
         int i = 0;
-        for (Iterator<String> it = strings.iterator(); it.hasNext(); ) {
-            String string = it.next();
-            ret[i] = Float.parseFloat(string);
-            i++;
+        for (String string : strings) {
+            ret[i++] = Float.parseFloat(string);
         }
         return ret;
     }
 
+    /**
+     * Convert a {@link String} {@link Collection} containing doubles to a {@link String} array.
+     * Convert all entries using {@link Double#parseDouble(String)}.
+     * <code>null</code> values in the collection are not allowed and throw a {@link NullPointerException}.
+     * @param strings The {@link String}s. Can be <code>null</code>.
+     * @return The double array. Never be <code>null</code>, may be empty.
+     */
     public double[] stringCollectionToDoubleArray(Collection<String> strings) {
+        if(strings == null || strings.size() == 0) {
+            return EMPTY_ARRAY_DOUBLE;
+        }
         double[] ret = new double[strings.size()];
         int i = 0;
-        for (Iterator<String> it = strings.iterator(); it.hasNext(); ) {
-            String string = it.next();
-            ret[i] = Double.parseDouble(string);
-            i++;
+        for (String string : strings) {
+            ret[i++] = Double.parseDouble(string);
         }
         return ret;
     }
 
+    /**
+     * Convert a {@link String} {@link Collection} containing booleans to a {@link String} array.
+     * Convert all entries using {@link Boolean#parseBoolean(String)}.
+     * <code>null</code> values in the collection are allowed and will be converted to a <code>false</code> value.
+     * @param strings The {@link String}s. Can be <code>null</code>.
+     * @return The boolean array. Never be <code>null</code>, may be empty.
+     */
     public boolean[] stringCollectionToBooleanArray(Collection<String> strings) {
+        if(strings == null || strings.size() == 0) {
+            return EMPTY_ARRAY_BOOLEAN;
+        }
         boolean[] ret = new boolean[strings.size()];
         int i = 0;
-        for (Iterator<String> it = strings.iterator(); it.hasNext(); ) {
-            String string = it.next();
-            ret[i] = Boolean.parseBoolean(string);
-            i++;
+        for (String string : strings) {
+            ret[i++] = Boolean.parseBoolean(string);
         }
         return ret;
     }
 
+    /**
+     * Convert a {@link String} {@link Collection} containing chars to a {@link String} array.
+     * Convert all entries using {@link String#charAt(int)}.
+     * <code>null</code> values in the collection are not allowed and throw a {@link NullPointerException}.
+     * @param strings The {@link String}s. Can be <code>null</code>.
+     * @return The char array. Never be <code>null</code>, may be empty.
+     */
     public char[] stringCollectionToCharArray(Collection<String> strings) {
+        if(strings == null || strings.size() == 0) {
+            return EMPTY_ARRAY_CHAR;
+        }
         char[] ret = new char[strings.size()];
         int i = 0;
-        for (Iterator<String> it = strings.iterator(); it.hasNext(); ) {
-            String string = it.next();
-            ret[i] = (char) Integer.parseInt(string);
-            i++;
+        for (String string : strings) {
+            ret[i++] = string.charAt(0);
         }
         return ret;
     }
--- src/test/java/net/ladenthin/javacommons/StringCollectionToPrimitiveArrayTest.java
+++ src/test/java/net/ladenthin/javacommons/StringCollectionToPrimitiveArrayTest.java
@@ -23,74 +23,179 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
- *
+ * Test the class {@link StringCollectionToPrimitiveArray}.
  * @author Bernard Ladenthin bernard.ladenthin@gmail.com
  */
 public class StringCollectionToPrimitiveArrayTest {
 
-    @Test
-    void stringCollectionToStringArray() throws IOException {
-        String[] strings = new StringCollectionToPrimitiveArray().stringCollectionToStringArray(Arrays.asList("One", "Two", "Three"));
+    /**
+     * The class under test.
+     */
+    private final StringCollectionToPrimitiveArray cut = new StringCollectionToPrimitiveArray();
 
-        assertArrayEquals(new String[]{"One", "Two", "Three"}, strings);
+    @Test
+    void stringCollectionToStringArray_correctUsage() throws IOException {
+        assertAll(
+                () -> assertNotNull(cut.stringCollectionToStringArray(null)),
+                () -> assertEquals(0, cut.stringCollectionToStringArray(null).length),
+                () -> assertArrayEquals(new String[]{}, cut.stringCollectionToStringArray(Arrays.asList())),
+                () -> assertArrayEquals(new String[]{null}, cut.stringCollectionToStringArray(Arrays.asList((String)null))),
+                () -> assertArrayEquals(new String[]{"One"}, cut.stringCollectionToStringArray(Arrays.asList("One"))),
+                () -> assertArrayEquals(new String[]{"One", "Two"}, cut.stringCollectionToStringArray(Arrays.asList("One", "Two"))),
+                () -> assertArrayEquals(new String[]{"One", "Two", "Three"}, cut.stringCollectionToStringArray(Arrays.asList("One", "Two", "Three")))
+        );
     }
 
     @Test
-    void stringCollectionToByteArray() throws IOException {
-        byte[] bytes = new StringCollectionToPrimitiveArray().stringCollectionToByteArray(Arrays.asList("0", "1", "2"));
-
-        assertArrayEquals(new byte[]{(byte)0, (byte)1, (byte)2}, bytes);
+    void stringCollectionToByteArray_correctUsage() throws IOException {
+        assertAll(
+                () -> assertNotNull(cut.stringCollectionToByteArray(null)),
+                () -> assertEquals(0, cut.stringCollectionToByteArray(null).length),
+                () -> assertArrayEquals(new byte[]{}, cut.stringCollectionToByteArray(Arrays.asList())),
+                () -> assertArrayEquals(new byte[]{(byte)0}, cut.stringCollectionToByteArray(Arrays.asList("0"))),
+                () -> assertArrayEquals(new byte[]{(byte)0, (byte)1}, cut.stringCollectionToByteArray(Arrays.asList("0", "1"))),
+                () -> assertArrayEquals(new byte[]{(byte)0, (byte)1, (byte)2}, cut.stringCollectionToByteArray(Arrays.asList("0", "1", "2")))
+        );
     }
 
     @Test
-    void stringCollectionToShortArray() throws IOException {
-        short[] shorts = new StringCollectionToPrimitiveArray().stringCollectionToShortArray(Arrays.asList("0", "1", "2"));
-
-        assertArrayEquals(new short[]{(short)0, (short)1, (short)2}, shorts);
+    void stringCollectionToByteArray_wrongUsage() throws IOException {
+        assertThrows(NumberFormatException.class, () -> {
+            cut.stringCollectionToByteArray(Arrays.asList((String)null));
+        });
     }
 
     @Test
-    void stringCollectionToIntArray() throws IOException {
-        int[] ints = new StringCollectionToPrimitiveArray().stringCollectionToIntArray(Arrays.asList("0", "1", "2"));
-
-        assertArrayEquals(new int[]{(short)0, (short)1, (short)2}, ints);
+    void stringCollectionToShortArray_correctUsage() throws IOException {
+        assertAll(
+                () -> assertNotNull(cut.stringCollectionToShortArray(null)),
+                () -> assertEquals(0, cut.stringCollectionToShortArray(null).length),
+                () -> assertArrayEquals(new short[]{}, cut.stringCollectionToShortArray(Arrays.asList())),
+                () -> assertArrayEquals(new short[]{(short)0}, cut.stringCollectionToShortArray(Arrays.asList("0"))),
+                () -> assertArrayEquals(new short[]{(short)0, (short)1}, cut.stringCollectionToShortArray(Arrays.asList("0", "1"))),
+                () -> assertArrayEquals(new short[]{(short)0, (short)1, (short)2}, cut.stringCollectionToShortArray(Arrays.asList("0", "1", "2")))
+        );
     }
 
     @Test
-    void stringCollectionToLongArray() throws IOException {
-        long[] longs = new StringCollectionToPrimitiveArray().stringCollectionToLongArray(Arrays.asList("0", "1", "2"));
-
-        assertArrayEquals(new long[]{(short)0, (short)1, (short)2}, longs);
+    void stringCollectionToShortArray_wrongUsage() throws IOException {
+        assertThrows(NumberFormatException.class, () -> {
+            cut.stringCollectionToShortArray(Arrays.asList((String)null));
+        });
     }
 
     @Test
-    void stringCollectionToFloatArray() throws IOException {
-        float[] floats = new StringCollectionToPrimitiveArray().stringCollectionToFloatArray(Arrays.asList("0", "1.1", "2.2"));
-
-        assertArrayEquals(new float[]{0f, 1.1f, 2.2f}, floats);
+    void stringCollectionToIntArray_correctUsage() throws IOException {
+        assertAll(
+                () -> assertNotNull(cut.stringCollectionToIntArray(null)),
+                () -> assertEquals(0, cut.stringCollectionToIntArray(null).length),
+                () -> assertArrayEquals(new int[]{}, cut.stringCollectionToIntArray(Arrays.asList())),
+                () -> assertArrayEquals(new int[]{0}, cut.stringCollectionToIntArray(Arrays.asList("0"))),
+                () -> assertArrayEquals(new int[]{0, 1}, cut.stringCollectionToIntArray(Arrays.asList("0", "1"))),
+                () -> assertArrayEquals(new int[]{0, 1, 2}, cut.stringCollectionToIntArray(Arrays.asList("0", "1", "2")))
+        );
     }
 
     @Test
-    void stringCollectionToBooleanArray() throws IOException {
-        boolean[] booleans = new StringCollectionToPrimitiveArray().stringCollectionToBooleanArray(Arrays.asList("true", "false"));
-
-        assertArrayEquals(new boolean[]{true, false}, booleans);
+    void stringCollectionToIntArray_wrongUsage() throws IOException {
+        assertThrows(NumberFormatException.class, () -> {
+            cut.stringCollectionToIntArray(Arrays.asList((String)null));
+        });
     }
 
     @Test
-    void stringCollectionToDoubleArray() throws IOException {
-        double[] doubles = new StringCollectionToPrimitiveArray().stringCollectionToDoubleArray(Arrays.asList("0", "1.1", "2.2"));
-
-        assertArrayEquals(new double[]{0.0d, 1.1d, 2.2d}, doubles);
+    void stringCollectionToLongArray_correctUsage() throws IOException {
+        assertAll(
+                () -> assertNotNull(cut.stringCollectionToLongArray(null)),
+                () -> assertEquals(0, cut.stringCollectionToLongArray(null).length),
+                () -> assertArrayEquals(new long[]{}, cut.stringCollectionToLongArray(Arrays.asList())),
+                () -> assertArrayEquals(new long[]{0L}, cut.stringCollectionToLongArray(Arrays.asList("0"))),
+                () -> assertArrayEquals(new long[]{0L, 1L}, cut.stringCollectionToLongArray(Arrays.asList("0", "1"))),
+                () -> assertArrayEquals(new long[]{0L, 1L, 2L}, cut.stringCollectionToLongArray(Arrays.asList("0", "1", "2")))
+        );
     }
 
     @Test
-    void stringCollectionToCharArray() throws IOException {
-        char[] chars = new StringCollectionToPrimitiveArray().stringCollectionToCharArray(Arrays.asList("0", "1", "2"));
+    void stringCollectionToLongArray_wrongUsage() throws IOException {
+        assertThrows(NumberFormatException.class, () -> {
+            cut.stringCollectionToLongArray(Arrays.asList((String)null));
+        });
+    }
 
-        assertArrayEquals(new char[]{(char)0, (char)1, (char)2}, chars);
+    @Test
+    void stringCollectionToFloatArray_correctUsage() throws IOException {
+        assertAll(
+                () -> assertNotNull(cut.stringCollectionToFloatArray(null)),
+                () -> assertEquals(0, cut.stringCollectionToFloatArray(null).length),
+                () -> assertArrayEquals(new float[]{}, cut.stringCollectionToFloatArray(Arrays.asList())),
+                () -> assertArrayEquals(new float[]{0f}, cut.stringCollectionToFloatArray(Arrays.asList("0"))),
+                () -> assertArrayEquals(new float[]{0f, 1.1f}, cut.stringCollectionToFloatArray(Arrays.asList("0", "1.1"))),
+                () -> assertArrayEquals(new float[]{0f, 1.1f, 2.2f}, cut.stringCollectionToFloatArray(Arrays.asList("0", "1.1", "2.2")))
+        );
+    }
+
+    @Test
+    void stringCollectionToFloatArray_wrongUsage() throws IOException {
+        assertThrows(NullPointerException.class, () -> {
+            cut.stringCollectionToFloatArray(Arrays.asList((String)null));
+        });
+    }
+
+    @Test
+    void stringCollectionToBooleanArray_correctUsage() throws IOException {
+        assertAll(
+                () -> assertNotNull(cut.stringCollectionToBooleanArray(null)),
+                () -> assertEquals(0, cut.stringCollectionToBooleanArray(null).length),
+                () -> assertArrayEquals(new boolean[]{}, cut.stringCollectionToBooleanArray(Arrays.asList())),
+                () -> assertArrayEquals(new boolean[]{false}, cut.stringCollectionToBooleanArray(Arrays.asList((String)null))),
+                () -> assertArrayEquals(new boolean[]{false}, cut.stringCollectionToBooleanArray(Arrays.asList("false"))),
+                () -> assertArrayEquals(new boolean[]{false, true}, cut.stringCollectionToBooleanArray(Arrays.asList("false", "true"))),
+                () -> assertArrayEquals(new boolean[]{false, true, false}, cut.stringCollectionToBooleanArray(Arrays.asList("false", "true", "false")))
+        );
+    }
+
+    @Test
+    void stringCollectionToDoubleArray_correctUsage() throws IOException {
+        assertAll(
+                () -> assertNotNull(cut.stringCollectionToDoubleArray(null)),
+                () -> assertEquals(0, cut.stringCollectionToDoubleArray(null).length),
+                () -> assertArrayEquals(new double[]{}, cut.stringCollectionToDoubleArray(Arrays.asList())),
+                () -> assertArrayEquals(new double[]{0.0d}, cut.stringCollectionToDoubleArray(Arrays.asList("0"))),
+                () -> assertArrayEquals(new double[]{0.0d, 1.1d}, cut.stringCollectionToDoubleArray(Arrays.asList("0", "1.1"))),
+                () -> assertArrayEquals(new double[]{0.0d, 1.1d, 2.2d}, cut.stringCollectionToDoubleArray(Arrays.asList("0", "1.1", "2.2")))
+        );
+    }
+
+    @Test
+    void stringCollectionToDoubleArray_wrongUsage() throws IOException {
+        assertThrows(NullPointerException.class, () -> {
+            cut.stringCollectionToDoubleArray(Arrays.asList((String)null));
+        });
+    }
+
+    @Test
+    void stringCollectionToCharArray_correctUsage() throws IOException {
+        assertAll(
+                () -> assertNotNull(cut.stringCollectionToCharArray(null)),
+                () -> assertEquals(0, cut.stringCollectionToCharArray(null).length),
+                () -> assertArrayEquals(new char[]{}, cut.stringCollectionToCharArray(Arrays.asList())),
+                () -> assertArrayEquals(new char[]{'A'}, cut.stringCollectionToCharArray(Arrays.asList("A"))),
+                () -> assertArrayEquals(new char[]{'A', 'B'}, cut.stringCollectionToCharArray(Arrays.asList("A", "B"))),
+                () -> assertArrayEquals(new char[]{'A', 'B', 'C'}, cut.stringCollectionToCharArray(Arrays.asList("A", "B", "C"))),
+                () -> assertArrayEquals(new char[]{'A'}, cut.stringCollectionToCharArray(Arrays.asList("AX"))),
+                () -> assertArrayEquals(new char[]{'A', 'B'}, cut.stringCollectionToCharArray(Arrays.asList("AX", "BX"))),
+                () -> assertArrayEquals(new char[]{'A', 'B', 'C'}, cut.stringCollectionToCharArray(Arrays.asList("AX", "BX", "CX")))
+        );
+    }
+
+    @Test
+    void stringCollectionToCharArray_wrongUsage() throws IOException {
+        assertThrows(NullPointerException.class, () -> {
+            cut.stringCollectionToCharArray(Arrays.asList((String)null));
+        });
     }
 }
